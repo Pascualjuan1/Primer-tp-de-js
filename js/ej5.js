ded3cad53d0aa9d189c1d10a2de6f8a110349265
Juan <juancir18@gmail.com>
2024-10-12 16:52:54 -0300

    resolucion hasta el ej 7

---
+++ js/ej5.js
@@ -0,0 +1,7 @@
+/*Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt
+*/
+var dato1 = prompt('');
+var dato2 = prompt('');
+document.write(parseInt(dato1) * parseInt (dato2) );
---
+++ js/ej6.js
@@ -0,0 +1,18 @@
+/*Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+Ejemplo: 
+
+input: 15 , 3
+
+
+Output: El 15 es el número más grande
+*/
+
+num1 = prompt ('');
+num2 = prompt ('');
+
+if (num1 > num2){
+    document.write('el numero ' + num1 + ' es el mas grande');
+}else{
+    document.write('el numero ' + num2 + ' es el mas grande');
+
+};
